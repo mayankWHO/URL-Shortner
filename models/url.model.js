edc162bd1f00d95f06abbd41cc2cd8d4a8ed9549
Mayank <mahavirmayank111@gmail.com>
2025-12-25 03:47:43 +0530

    url table schema

---
+++ models/url.model.js
@@ -0,0 +1,15 @@
+import {pgTable,uuid,varchar, text, timestamp} from "drizzle-orm/pg-core";
+import { usersTable } from "./user.model.js";
+
+
+export const urlsTable = pgTable('urls', {
+    id: uuid('id').primaryKey(),
+    shortCode: varchar('code', {length:155}).notNull().unique(),
+    targetURL: text('target-url').notNull(),
+    userId: uuid('user_id')
+        .references(() => usersTable.id)
+        .notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+
+})
